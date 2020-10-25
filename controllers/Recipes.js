b475e69dbb121f126f8d014988ea555cbd465dd3
Jean Fernandes de Macedo <jfmacedo91@gmail.com>
2020-10-25 11:38:59 -0300

    added index page od admin

--- controllers/Recipes.js
+++ controllers/Recipes.js
@@ -1,12 +1,11 @@
-const recipes = require('../data')
+const data = require('../data.json')
 
 exports.index = (req, res) => {
-  return res.render('recipes', { recipes })
+  return res.render('admin/index', { recipes: data.recipes })
 }
-
-exports.show = (req, res) => {
-  const index = req.params.index;
-  const recipe = recipes[index]
-
-  res.render('recipe', { recipe })
-}
+exports.create = {}
+exports.show = {}
+exports.edit = {}
+exports.port = {}
+exports.put = {}
+exports.delete = {}
--- routes.js
+++ routes.js
@@ -11,7 +11,23 @@ routes.get('/', (req, res) => {
 routes.get('/about', (req, res) => {
   return res.render('about')
 })
-routes.get('/recipes', RecipesController.index)
-routes.get('/recipes/:index', RecipesController.show)
+routes.get('/recipes', (req, res) => {
+  return res.render('recipes', { recipes })
+})
+routes.get('/recipes/:index', (req, res) => {
+  const index = req.params.index;
+  const recipe = recipes[index]
+
+  res.render('recipe', { recipe })
+})
+
+routes.get('/admin/recipes', RecipesController.index)
+// routes.get('/admin/recipes/create', RecipesController.create)
+// routes.get('/admin/recipes/:id', RecipesController.show)
+// routes.get('/admin/recipes/:id/edit', RecipesController.edit)
+
+// routes.post('/admin/recipes', RecipesController.post)
+// routes.put('/admin/recipes', RecipesController.put)
+// routes.delete('/admin/recipes', RecipesController.delete)
 
 module.exports = routes
